9001524765d9aa2c59d6aa4e7f7ac10555fd4226
hello <hello@DREAM>
2023-11-13 06:30:43 +0800

    修改头部

--- YaoHuo.Plugin/Tool/WebTool.cs
+++ YaoHuo.Plugin/Tool/WebTool.cs
@@ -629,10 +629,10 @@ namespace YaoHuo.Plugin.Tool
             }
             else
             {
-                stringBuilder.Append("<!DOCTYPE html><html>\n");
+                stringBuilder.Append("<!DOCTYPE html><html>");
                 stringBuilder.Append("<head>\n");
-                stringBuilder.Append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>\n");
-                stringBuilder.Append("<meta http-equiv=\"Cache-Control\" content=\"no-cache\"/>\n");
+                //stringBuilder.Append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>\n");
+                //stringBuilder.Append("<meta http-equiv=\"Cache-Control\" content=\"no-cache\"/>\n");
                 if (wmlVo.mycss.IndexOf("viewport") < 0)
                 {
                     stringBuilder.Append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0\">\n");
